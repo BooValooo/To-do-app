829cede7b77d6e5abb148229a1fffb91df6c8909
ybzp <150625978+ybzp@users.noreply.github.com>
2024-02-04 21:56:46 +0100

    Refactor to have Multi-page

--- EaseTask/App.js
+++ EaseTask/App.js
@@ -1,66 +1,11 @@
-// App.js
-import React from 'react';
-import { View, ScrollView, StyleSheet } from 'react-native';
-import Headbar from './Src/Components/Headbar';
-import TaskBox from './Src/Components/TaskBox'; 
+import React from 'react'
+import AppContainer from './Src/Components/app-container'
+import Navigator from './Src/'
 
-const App = () => {
-  const handleIcon1Press = () => {
-    console.log('Search icon pressed');
-  };
-
-  const handleIcon2Press = () => {
-    console.log('Options icon pressed');
-  };
-
-  const handleCheckPress = (taskId) => {
-    console.log(`Task ${taskId} checkbox pressed`);
-  };
-
-  const handleMenuPress = (taskId) => {
-    console.log(`Task ${taskId} menu pressed`);
-  };
-
-  const tasks = [
-    { id: 1, name: 'Task 1', priority: 'Priority 1', time: '08:30 PM', isChecked: false },
-    { id: 2, name: 'Task 2', priority: 'Priority 1', time: '08:30 PM', isChecked: false },
-    { id: 3, name: 'Task 3', priority: 'Priority 3', time: '08:30 PM', isChecked: true },
-  ];
-
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Headbar
-        showSearchIcon={true}
-        headbarText="New Task"
-        onSearchPress={handleIcon1Press}
-        onOptionsPress={handleIcon2Press}
-      />
-      <ScrollView style={styles.taskList}>
-        {tasks.map((task) => (
-          <TaskBox
-            key={task.id}
-            taskName={task.name}
-            priority={task.priority}
-            time={task.time}
-            isChecked={task.isChecked}
-            onCheckPress={() => handleCheckPress(task.id)}
-            onMenuPress={() => handleMenuPress(task.id)}
-          />
-        ))}
-      </ScrollView>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  taskList: {
-    flex: 1,
-    marginTop: 20, // 为任务列表提供一些顶部空间
-  },
-});
-
-export default App;
+    <AppContainer>
+      <Navigator />
+    </AppContainer>
+  )
+}
---
+++ EaseTask/Src/Views/test.tsx
@@ -0,0 +1,36 @@
+// App.js
+
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Headbar from '../Components/Headbar';
+
+const Test = () => {
+  const handleIcon1Press = () => {
+    // Handle the press event for the first icon
+    console.log('Icon 1 pressed');
+  };
+
+  const handleIcon2Press = () => {
+    // Handle the press event for the second icon
+    console.log('Icon 2 pressed');
+  };
+
+  const showSearchIcon = true;
+  const headbarText = "New task"
+
+  return (
+    <View style={styles.container}>
+      <Headbar showSearchIcon={showSearchIcon} headbarText={headbarText} onSearchPress={handleIcon1Press} onOptionsPress={handleIcon2Press} />
+      {/* Add the rest of your app components below */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+export default Test;
