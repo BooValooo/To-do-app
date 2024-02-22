30de13ef3ee17b0aa077cc34fe5f568cd2b8cb39
Valentin Abgrall <153991458+BooValooo@users.noreply.github.com>
2024-02-22 11:10:57 +0100

    Update index.tsx

--- EaseTask/src/index.tsx
+++ EaseTask/src/index.tsx
@@ -23,7 +23,7 @@ const Navigator = () => {
               tabBarInactiveTintColor: 'black',
               tabBarShowLabel: false,
               headerShown: false,
-              tabBarStyle: { position: 'absolute', backgroundColor: '#ababab' }, // Add backgroundColour only for Android
+              tabBarStyle: { position: 'absolute', backgroundColor: 'rgba(255, 255, 255, 0.75)' },              
               tabBarBackground: () => (
                 <BlurView tint="light" intensity={50} style={StyleSheet.absoluteFill} />
               ),
