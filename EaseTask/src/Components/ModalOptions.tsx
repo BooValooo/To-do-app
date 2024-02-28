8fbeb8bafa90a085aa7e6b1aa5e17acaea5c34b0
ybzp <150625978+ybzp@users.noreply.github.com>
2024-02-28 09:09:51 +0100

    Update Chat

--- EaseTask/src/Components/ModalOptions.tsx
+++ EaseTask/src/Components/ModalOptions.tsx
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
   },
   modalContent: {
     backgroundColor: 'white',
-    paddingTop: 20,
+    // paddingTop: 20,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
     width: '100%',
