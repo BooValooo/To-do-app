6b26416405331cf6a503b9651db08615f65d718f
BooValooo <abgrall.valentin@yahoo.com>
2024-02-28 16:55:51 +0100

    The user can now swipe right or left to delete a task or a note.

--- EaseTask/src/Components/TaskBox.tsx
+++ EaseTask/src/Components/TaskBox.tsx
@@ -39,6 +39,27 @@ const TaskBox = ({ task, onCheckPress, onMenuPress, onDelete }) => {
     );
   };
 
+  const renderLeftActions = (progress, dragX) => {
+    const scale = dragX.interpolate({
+      inputRange: [0, 100],
+      outputRange: [0, 1],
+      extrapolate: 'clamp',
+    });
+
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          swipeableRow.close(); 
+          onDelete(task.id); 
+        }}
+        style={styles.deleteButton}>
+        <Animated.Text style={[styles.deleteButtonText, { transform: [{ scale }] }]}>
+          Delete
+        </Animated.Text>
+      </TouchableOpacity>
+    );
+  };
+
 
   const handleMenuPressInternal = () => {
     setModalVisible(true);
@@ -53,6 +74,7 @@ const TaskBox = ({ task, onCheckPress, onMenuPress, onDelete }) => {
     <Swipeable
       ref={(ref) => setSwipeableRow(ref)}
       renderRightActions={renderRightActions}
+      renderLeftActions={renderLeftActions}
       rightThreshold={40}
     >
       <View style={[styles.taskContainer, { minHeight: minHeight }]}>
